bf8bba567ed86c70584902469273218848927e92
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2025-09-07 12:26:04 +0200

    layout

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,29 @@
+import {
+    SidebarInset,
+    SidebarProvider,
+} from "@/components/ui/sidebar"
+import { Header } from "@/components/dashboard/header"
+import { DashboardSidebar } from "@/components/dashboard/sidebar"
+import ProtectedPage from '@/components/auth/ProtectedPage'
+
+
+export default function DashboardLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <ProtectedPage>
+            <SidebarProvider>
+                <DashboardSidebar />
+                <SidebarInset>
+                    <div className="sticky top-0 z-50 bg-background border-b">
+                        <Header />
+                    </div>
+
+                    {children}
+                </SidebarInset>
+            </SidebarProvider>
+        </ProtectedPage>
+    )
+}
