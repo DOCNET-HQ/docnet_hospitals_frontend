097c708d9d2f198467155e5c7fafd33b0069a462
Abimbola Ronald <r.abimbola@alustudent.com>
2025-10-12 23:57:21 +0400

    Added the messages page

---
+++ src/app/(dashboard)/messages/page.tsx
@@ -0,0 +1,11 @@
+import { ChatMain } from "@/components/chat/chat-main";
+import { ChatSidebar } from "@/components/chat/chat-sidebar";
+
+export default function Page() {
+  return (
+    <div className="flex h-[calc(100vh-60px)] lg:px-4 -mt-3 justify-center">
+      <ChatSidebar />
+      <ChatMain />
+    </div>
+  );
+}
