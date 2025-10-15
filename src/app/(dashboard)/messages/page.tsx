0cd4b08d1a6dc836f5adbebbe634b516dbd8b784
Abimbola Ronald <r.abimbola@alustudent.com>
2025-10-16 01:05:38 +0400

    Modified the messages page

--- src/app/(dashboard)/messages/page.tsx
+++ src/app/(dashboard)/messages/page.tsx
@@ -3,7 +3,7 @@ import { ChatSidebar } from "@/components/chat/chat-sidebar";
 
 export default function Page() {
   return (
-    <div className="flex h-[calc(100vh-60px)] lg:px-4 -mt-3 justify-center">
+    <div className="flex h-[calc(100vh-50px)] lg:px-4 -mt-3 justify-center">
       <ChatSidebar />
       <ChatMain />
     </div>
