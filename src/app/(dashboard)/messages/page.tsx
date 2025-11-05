66f346fedb5ee1b17178af0cffae16f194dfbe3f
Abimbola Ronald <r.abimbola@alustudent.com>
2025-11-05 10:46:09 +0400

    Modified

--- src/app/(dashboard)/messages/page.tsx
+++ src/app/(dashboard)/messages/page.tsx
@@ -1,11 +1,22 @@
-import { ChatMain } from "@/components/chat/chat-main";
-import { ChatSidebar } from "@/components/chat/chat-sidebar";
+
 
 export default function Page() {
   return (
-    <div className="flex h-[calc(100vh-50px)] lg:px-4 -mt-3 justify-center">
-      <ChatSidebar />
-      <ChatMain />
+    <div
+      className="w-[90%] flex flex-col items-center justify-center"
+    >
+      <div
+        className="text-2xl font-semibold mb-4"
+      >
+        Start a conversation
+      </div>
+      <img
+        src="https://cdn.prod.website-files.com/5d015870ec9646043c2f3127/5ebc2a49a428098290267716_ezgif.com-optimize%20(18).gif"
+        className="object-cover"
+        width={500}
+        height={400}
+        alt=""
+      />
     </div>
   );
 }
